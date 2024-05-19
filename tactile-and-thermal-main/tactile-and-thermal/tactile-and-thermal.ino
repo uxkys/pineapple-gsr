14d5c8540471be1124aefdbd5527784cda4d1ec5
munenori.koyasu <123240216+uxkys@users.noreply.github.com>
2024-05-19 20:55:23 +0900

    Update tactile-and-thermal.ino
    GSRセンサだけ動かしてみる

--- tactile-and-thermal-main/tactile-and-thermal/tactile-and-thermal.ino
+++ tactile-and-thermal-main/tactile-and-thermal/tactile-and-thermal.ino
@@ -3,46 +3,24 @@
 
 #include "pineapple.h"
 
-const int sensor_bits = 9;
-const int sensor_pin = A5;
-
-OneWire oneWire(sensor_pin);
-DallasTemperature ths(&oneWire);
+const int GSR = ANLG1;
+int sensorValue = 0;
+int gsr_average = 0;
 
 void setup() {
   pinMode(GATE, OUTPUT);
   digitalWrite(GATE, HIGH);
   Serial.begin(9600);
-  ths.setResolution(sensor_bits);
 }
 
-void show_temp() {
-  ths.requestTemperatures();
-  Serial.print("THS, ");
-  Serial.print(ths.getTempCByIndex(0));
+void loop(){
+  long sum=0;
+  for(int i=0;i<10;i++) //Average the 10 measurements to remove the glitch
+    {
+    sensorValue=analogRead(GSR);
+    sum += sensorValue;
+    delay(5);
+  }
+  gsr_average = sum/10;
+  Serial.println(gsr_average);
 }
-
-void show_press() {
-  int p;
-  p = analogRead(ANLG0);
-  Serial.print("ANLG0, ");
-  Serial.print(p);
-}
-
-void show_gsr() {
-  int g;
-  g = analogRead(ANLG1);
-  Serial.print("ANLG1, ");
-  Serial.print(g);
-}
-
-void loop() {
-  show_temp();
-  Serial.print(", ");
-  show_press();
-  Serial.print(", ");
-  show_gsr();
-  Serial.println("");
-  delay(1000); // 次のループまで1秒待機
-}
-
