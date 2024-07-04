4dcca55c267d182d85ee49b1ef723401899c36c9
munenori.koyasu <123240216+uxkys@users.noreply.github.com>
2024-07-04 20:35:45 +0900

    Update tactile-and-thermal.ino
    analogReadの生データを100倍

--- tactile-and-thermal-main/tactile-and-thermal/tactile-and-thermal.ino
+++ tactile-and-thermal-main/tactile-and-thermal/tactile-and-thermal.ino
@@ -17,41 +17,16 @@ void setup() {
   ths.setResolution(sensor_bits);
 }
 
-void show_temp() {
-  //ths.requestTemperatures();
-  //Serial.print("THS, ");
-  //Serial.print(ths.getTempCByIndex(0));
-}
-
-void show_press() {
-  //int p;
-  //p = analogRead(ANLG0);
-  //Serial.print("ANLG0, ");
-  //Serial.print(p);
-}
-
 void show_gsr() {
   int sensorValue = 0;
   long sum = 0;
   int gsr_average = 0;
-
-  for(int i = 0; i < 10; i++) {  // Average the 10 measurements to remove the glitch
-    sensorValue = analogRead(GSR) * 100;  // Scale the value by 100
-    sum += sensorValue;
-    delay(5);
-  }
-  gsr_average = sum / 10;
-  // int last_three_digits = gsr_average % 1000;  // Extract the last three digits
-  int scaled_value = gsr_average;
-  Serial.print("GSR, ");
-  Serial.println(scaled_value);  // Print the scaled value
+  sensorValue = analogRead(GSR) * 100;
+  Serial.println(sensorValue);
 }
 
 void loop() {
-  //show_temp();
-  //Serial.print(", ");
-  //show_press();
   show_gsr();
   Serial.println("");
-  delay(100);  // 次のデータ取得まで100ミリ秒待機
+  delay(1000);  // 次のデータ取得まで100ミリ秒待機
 }
